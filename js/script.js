574ccc35f842e62a0a7abbf652bfb6d514e01670
Vladimir <vladimir1@ukr.net>
2021-11-14 10:39:56 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+"use strict"
+
+// alert("Hello!");
+
+// const result = confirm("Are you sure?");
+// console.log(result);
+
+// const answer = prompt ("How old are you?","20");
+// console.log (answer, typeof(answer));
+
+// const user = "Саша";
+// alert (`Привет ${user}`);
+
+console.log (2*4 == 8);
